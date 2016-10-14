5c49502f988e1f6ab305c94134f2ebd501d3987c
Mark Lawson <mlawson3691@gmail.com>
2016-10-14 09:44:46 -0700

    refactor error display logic

--- js/user.js
+++ js/user.js
@@ -9,19 +9,18 @@ User.prototype.getRepos = function() {
   var _this = this;
   $.get('https://api.github.com/users/' + _this.username + '/repos?sort=created&access_token=' + apiKey).then(function(response) {
     console.log(response);
-    if (response !== null) {
-      response.forEach(function(repo) {
-        $('#repos').append('<li><a href="' + repo.svn_url + '" target="_blank"><h4>' + repo.name + '</h4></a></li>');
-        if (repo.description) {
-          $('li').last().append('<p>' + repo.description + '</p>');
-        }
-        $('div.hidden').removeClass('hidden');
-      });
-    }
+    response.forEach(function(repo) {
+      $('#repos').append('<li><a href="' + repo.svn_url + '" target="_blank"><h4>' + repo.name + '</h4></a></li>');
+      if (repo.description) {
+        $('li').last().append('<p>' + repo.description + '</p>');
+      }
+    });
+    $('#result').show();
+    $('#error').hide();
   }).fail(function(error) {
     console.log(error.responseJSON.message);
-    $('#hidden-div').html('That username does not exist. Try again!');
-    $('div.hidden').removeClass('hidden');
+    $('#error').show();
+    $('#result').hide();
   });
 };
 
