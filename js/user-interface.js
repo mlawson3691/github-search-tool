020274c7995e6e4745315f04e650faec33c56ef7
Mark Lawson <mlawson3691@gmail.com>
2016-10-14 11:15:10 -0700

    add callback functions to move jQuery from back-end to interface

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,12 +1,40 @@
 var User = require('./../js/user.js').userModule;
 
+var displayUser = function(user) {
+  console.log(user);
+
+  $('#user-avatar').attr('src', user.avatar_url);
+  $('#user').parent().attr('href', user.html_url);
+}
+
+var displayRepos = function(repos) {
+  $('#repos1').empty();
+  $('#repos2').empty();
+  var id;
+  repos.forEach(function(repo, i) {
+    if (i < repos.length/2) {
+      id = '#repos1';
+    } else {
+      id = '#repos2';
+    }
+    $(id).append('<div class="well"><a href="' + repo.svn_url + '" target="_blank"><h4>' + repo.name + '</h4></a></div>');
+    if (repo.description) {
+      $('.well').last().append('<p>' + repo.description + '</p>');
+    }
+  });
+  $('#result').show();
+  $('#error').hide();
+}
+
 $(document).ready(function() {
+
   $('#submit').click(function(event) {
     event.preventDefault();
+    $('#load-text').hide();
     var username = $('#username').val();
     $('#username').val('');
     $('#user').text(username);
     var newUser = new User(username);
-    newUser.getRepos();
+    newUser.getRepos(displayUser, displayRepos);
   });
 });
--- js/user.js
+++ js/user.js
@@ -5,27 +5,14 @@ function User(username) {
   this.repos = [];
 }
 
-User.prototype.getRepos = function() {
+User.prototype.getRepos = function(displayUser, displayRepos) {
   var _this = this;
   $.get('https://api.github.com/users/' + _this.username + '/repos?sort=created&access_token=' + apiKey).then(function(response) {
-    console.log(response);
-    console.log(response.length);
-    $('#repos1').empty();
-    $('#repos2').empty();
-    var id;
-    response.forEach(function(repo, i) {
-      if (i < response.length/2) {
-        id = '#repos1';
-      } else {
-        id = '#repos2';
-      }
-      $(id).append('<div class="well"><a href="' + repo.svn_url + '" target="_blank"><h4>' + repo.name + '</h4></a></div>');
-      if (repo.description) {
-        $('.well').last().append('<p>' + repo.description + '</p>');
-      }
-    });
-    $('#result').show();
-    $('#error').hide();
+    // console.log(response);
+
+    displayUser(response["0"].owner);
+    displayRepos(response);
+
   }).fail(function(error) {
     console.log(error.responseJSON.message);
     $('#error').show();
